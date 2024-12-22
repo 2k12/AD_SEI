9de4e7ad4ee0574bf92063fd2b658f825cbe2d03
Joan Pastillo <jeipige@gmail.com>
2024-12-22 12:42:29 -0500

    update docs

--- docs/docs.go
+++ docs/docs.go
@@ -508,7 +508,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "${SWAGGER_HOST}",
+	Host:             "seri-api-utn-2024.fly.dev",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "API SEGURIDAD con Swagger",
