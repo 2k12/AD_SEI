51142f67aca5d28ed6bb56a4ae8dfe7185edf297
JAPASPUELS <japaspuels@utn.edu.ec>
2025-01-02 17:46:38 -0500

    Actualizacion de Crud Roles

--- controllers/role_controller.go
+++ controllers/role_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"seguridad-api/services"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,7 @@ func CreateRole(c *gin.Context) {
 	var input struct {
 		Name        string `json:"name" binding:"required"`
 		Description string `json:"description"`
+		Active      bool   `json:"active"`
 	}
 
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -35,4 +37,34 @@ func GetRoles(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"roles": roles})
+}
+
+func UpdateRole(c *gin.Context) {
+	// Obtener el ID del rol desde los parámetros de la URL
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID inválido"})
+		return
+	}
+
+	var input struct {
+		Name        string `json:"name" binding:"required"`
+		Description string `json:"description"`
+		Active      bool   `json:"active"`
+	}
+
+	// Validar y enlazar los datos del cuerpo de la solicitud
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Llamar al servicio para actualizar el rol
+	role, err := services.UpdateRole(id, input.Name, input.Description, input.Active)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"role": role})
 }
