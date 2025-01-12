d360e37e51afbec161ebc9d166803b6b10d0b880
Joan Pastillo <jeipige@gmail.com>
2025-01-11 23:47:05 -0500

    added excel reports into user reports modal

--- controllers/reports/report_controller.go
+++ controllers/reports/report_controller.go
@@ -7,28 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func GenerateReport(c *gin.Context) {
-// 	var requestData struct {
-// 		Filters  map[string]interface{} `json:"filters"`
-// 		Model    string                 `json:"model"`
-// 		Username string                 `json:"username"`
-// 	}
-
-// 	if err := c.ShouldBindJSON(&requestData); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Datos inválidos"})
-// 		return
-// 	}
-
-// 	fileBuffer, fileName, err := services.GenerateReport(requestData.Model, requestData.Filters, requestData.Username)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	c.DataFromReader(http.StatusOK, int64(fileBuffer.Len()), "application/pdf", fileBuffer, map[string]string{
-// 		"Content-Disposition": `attachment; filename="` + fileName + `"`})
-// }
-
 func GenerateReport(c *gin.Context) {
 	var requestData struct {
 		Filters  map[string]interface{} `json:"filters"`
